2133a29559af99503afc2ca9bfca3a15ccb5ea7d
AjrinNR <ajrinnr11@gmail.com>
2019-08-01 16:41:20 +0700

    tugas1

---
+++ tugas1.php
@@ -0,0 +1,15 @@
+<?php
+
+    $harga = 15000;
+    $dus = 6;
+    $stpdus = 5;
+    $hrgstpdus = 2000;
+    $jml2 = $harga * ($dus * $stpdus);
+    $jml3 = $hrgstpdus * $dus;
+    $jml4 = $jml2 + $jml3;
+
+
+    echo "$jml4";
+
+
+?>
